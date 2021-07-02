59d61d6418448aa270d4983c6d8d9ce4048093e6
1004hjpark <1004hjpark@gmail.com>
2021-07-02 17:30:49 +0900

    빵+쨈+빵+쨈 (여러번 추가)

--- sandwich.go
+++ sandwich.go
@@ -5,10 +5,10 @@ import "fmt"
 type Bread struct {
 	val string
 }
-type StrawberryJam struct {
+type OrangeJam struct {
 	opend bool
 }
-type SpoonOfStrawberry struct {
+type SpoonOfOrange struct { //구조체
 }
 type sandwich struct {
 	val string
@@ -22,16 +22,18 @@ func GetBreads(num int) []*Bread {
 	}
 	return breads
 }
-func openStrawberryJam(jam *StrawberryJam) {
+func openOrangeJam(jam *OrangeJam) {
 	jam.opend = true
 }
 
-func GetOneSpoon(_ *StrawberryJam) *SpoonOfStrawberry {
-	return &SpoonOfStrawberry{}
+func GetOneSpoon(_ *OrangeJam) *SpoonOfOrange {
+	return &SpoonOfOrange{}
 
 }
-func PutJamOnBread(bread *Bread, jam *SpoonOfStrawberry) {
-	bread.val += "+ Strawberry Jam(쨈)"
+func PutJamOnBread(bread *Bread, jam *SpoonOfOrange) {
+	for i := 0; i < 1; i++ { //쨈 바르는 횟수 수정하는 곳
+		bread.val += "+ Orange Jam(쨈)"
+	}
 
 }
 
@@ -45,23 +47,24 @@ func MakeSandwich(bread []*Bread) *sandwich {
 
 func main() {
 
-	breads := GetBreads(2)
+	breads := GetBreads(10) // 빵 두개 꺼내기
 
-	jam := &StrawberryJam{} //잼을열고
+	jam := &OrangeJam{}
 
-	openStrawberryJam(jam) //딸기잼 뚜껑열기
+	openOrangeJam(jam) //딸기잼 뚜껑열기
 
-	spoon := GetOneSpoon(jam)
+	spoon := GetOneSpoon(jam) //빵 위에 올린다
 
-	PutJamOnBread(breads[0], spoon)
-	//PutJamOnBread(breads[0], spoon)
-	PutJamOnBread(breads[1], spoon) //잼을 한번만 더 바를때
-	// for i := 0; i < len(breads); i++ {
-	// 	PutJamOnBread(breads[i], spoon)
-	// }
+	// PutJamOnBread(breads[0], spoon) //잼을 바른다
+	// PutJamOnBread(breads[0], spoon) //잼을 한번만 더 바를때
+	for i := 0; i < len(breads); i++ {
+		PutJamOnBread(breads[i], spoon)
+		//빵 갯수에 따라 쨈의 갯수가 달라지기 때문에
+		//50번째 라인 getBreads(숫자)의 숫자를 수정해야함
+	}
 
-	sandwich := MakeSandwich(breads)
+	sandwich := MakeSandwich(breads) //빵을 덮는다
 	//jam더 올리기
 
-	fmt.Println(sandwich.val)
+	fmt.Println(sandwich.val) //완성
 }
